381010f3b2325bbff8c7d3b94eb54728689115be
srplanta <srplanta@gmail.com>
2023-08-06 11:34:12 +0500

    some code is cleaned

--- GeneralParactice/Program.cs
+++ GeneralParactice/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿//NOTES
 //Teacher inherits Employee and implements INotification at the same time
 //Octagon demonstrates explicit implementation of IDrawToForm, IDrawToMemory, IDrawToPrinter
 //DrawUpsideDown implements all members in hierarcy (IAdvancedDraw which inherit from IDrawable)
@@ -11,29 +11,21 @@ internal class Program
     private static void Main(string[] args)
     {
         ////Teacher inherits Employee and implements INotification at the same time
+        ////not any active functionality
         //Teacher teacher = new Teacher();    //teacher is inherited from Employee class
         //teacher.Id = 1;
         //####################################################################################
 
         ////Octagon demonstrates explicit implementation of IDrawToForm, IDrawToMemory, IDrawToPrinter
-        //Octagon octagon = new Octagon();
-        //IDrawToForm drawToForm = (IDrawToForm)octagon;
-        //drawToForm.Draw();
-        //IDrawToMemory drawToMemory = (IDrawToMemory)octagon;
-        //drawToMemory.Draw();
-        //IDrawToPrinter drawToPrinter = (IDrawToPrinter)octagon;
-        //drawToPrinter.Draw();
-        //---------------------------------------------------------------------
-
-        //IDrawToForm drawToForm = new Octagon();         //Explicitly implemented interface
-        //IDrawToMemory drawToMemory = new Octagon();     //Explicitly implemented interface
-        //IDrawToPrinter printToPrinter = new Octagon();  //Explicitly implemented interface
-        //drawToForm.Draw();
-        //drawToMemory.Draw();
-        //printToPrinter.Draw();
+        IDrawToForm drawToForm = new Octagon();         //Explicitly implemented interface
+        IDrawToMemory drawToMemory = new Octagon();     //Explicitly implemented interface
+        IDrawToPrinter printToPrinter = new Octagon();  //Explicitly implemented interface
+        drawToForm.Draw();
+        drawToMemory.Draw();
+        printToPrinter.Draw();
         //####################################################################################
 
-        ////DrawUpsideDown implements all members in hierarcy (IAdvancedDraw which inherit from IDrawable)
+        ////DrawUpsideDown implements all members in hierarcy (IAdvancedDraw extends IDrawable)
         BitmapImage bitmapImage = new BitmapImage();
         bitmapImage.Draw();
         bitmapImage.DrawInBoundingBox(3, 4, 5, 6);
